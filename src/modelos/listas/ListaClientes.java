1a76a420dc3cfcaf2af8d59f7861ed8f097c8bbe
Roberto <rh4849884@gmail.com>
2023-12-05 22:33:21 -0500

    Cambios en el metodo de validacion de cajas de texto

--- src/modelos/listas/ListaClientes.java
+++ src/modelos/listas/ListaClientes.java
@@ -202,7 +202,8 @@ public class ListaClientes implements Utilidades {
 
     @Override
     public boolean cajasVacias() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        return true;
     }
 
 }
