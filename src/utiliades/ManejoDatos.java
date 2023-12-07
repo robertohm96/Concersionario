a2ba94a48372fdce5d546528f2a6af53bf7c90cd
roberto <rh4849884@gmail.com>
2023-12-07 09:00:16 -0500

    Modificacion de la clase del manejo de datos

--- src/utiliades/ManejoDatos.java
+++ src/utiliades/ManejoDatos.java
@@ -16,6 +16,8 @@ import javax.swing.JOptionPane;
 import modelos.Cliente;
 import modelos.colas.ColaHistorialCompra;
 import modelos.listas.ListaClientes;
+import modelos.listas.ListaVehiculos;
+import modelos.pilas.Pilas;
 
 /**
  *
@@ -26,10 +28,14 @@ public class ManejoDatos implements Utilidades {
     private static ManejoDatos manejoDatos;
     private final ColaHistorialCompra colaHistorialCompra;
     private final ListaClientes listaClientes;
+    private final ListaVehiculos listaVehiculos;
+    private final Pilas pilas;
 
     public ManejoDatos() {
         colaHistorialCompra = new ColaHistorialCompra();
         listaClientes = new ListaClientes();
+        listaVehiculos = new ListaVehiculos();
+        this.pilas = new Pilas();
     }
 
     public static ManejoDatos getDatos() {
@@ -37,6 +43,10 @@ public class ManejoDatos implements Utilidades {
             manejoDatos = new ManejoDatos();
         }
         return manejoDatos;
+    }
+
+    public Pilas getPilas() {
+        return pilas;
     }
 
     public ColaHistorialCompra getColaHistorialCompra() {
@@ -47,77 +57,8 @@ public class ManejoDatos implements Utilidades {
         return listaClientes;
     }
 
-    public void leerClientes() {
-        String linea = ""; // Variable para guardar la línea leída.
-        try {
-            File directorio = new File(ManejoDatos.getDatos()
-                    .getListaClientes()
-                    .getRuta().replace("\\ListaClientes.txt", ""));/*
-            se crear una variable guardar la ruta para  la carpeta que guardara los archivos 
-            utilizados en el programa u tilizamos el metodo replace(cadena a cambiar,cadena nueva), para
-            dejar la ruta hasta la carpeta que almacenara los archivos.txt
-             */
-            if (!directorio.exists()) {/*Verificamos si no existe el directorio que se creara
-                en la ruta especificada, si no exite entra en la ruta especificada*/
-                if (directorio.mkdirs()) {/*Creamos el directorio o carpeta con el metodo mkdirs()
-                    y validamos si se creo correctamente metiendo el comando
-                    en un if, si devuelve true mandara un mensaje de confirmacion*/
-                    mostrarAlerta(Alert.AlertType.CONFIRMATION, "Directorio creado", "Felicitacions");
-                } else {
-                    //Si no mandara un mensaje de error
-                    mostrarAlerta(Alert.AlertType.CONFIRMATION,
-                            "la carpeta donde se\n guardaran los archivos.txt no existe",
-                            "Error al crear directorio");
-                }
-            }
-            FileReader archivo = new FileReader(listaClientes.getRuta());
-            BufferedReader leer = new BufferedReader(archivo);
-            while ((linea = leer.readLine()) != null) {
-                String[] datos = linea.split(" - "); // Separa los datos leídos por guiones y los guarda en un arreglo.
-                Cliente cliente = new Cliente(Long.parseLong(datos[0]),
-                        Long.parseLong(datos[1]), datos[2], datos[3],
-                        datos[4], datos[5]);
-                listaClientes.setAddFin(cliente);
-//                listaCircularClientes.cargarInfo(getVistaVerClientes().getTbClientes());
-            }
-            leer.close();
-        } catch (FileNotFoundException e) {
-            try {
-                File archivo = new File(listaClientes.getRuta());
-                archivo.createNewFile();
-            } catch (IOException ex) {
-                mostrarAlerta(Alert.AlertType.ERROR,
-                        "Error al crear el archivo.",
-                        "Error");
-            }
-        } catch (IOException e) {
-            mostrarAlerta(Alert.AlertType.ERROR,
-                    "Error al leer el archivo.",
-                    "Error");
-
-        }
-    }
-
-    public void guardarCliente(Cliente cliente) {
-        BufferedWriter escribir;
-        try {
-            File archivo = new File(listaClientes.getRuta());
-            if (archivo.exists()) {
-                escribir = new BufferedWriter(new FileWriter(archivo, true));
-            } else {
-                escribir = new BufferedWriter(new FileWriter(archivo));
-            }
-            if (cliente != null) {
-                escribir.write(cliente.getIdentificacion() + " - " + cliente.getNombre() + " - "
-                        + cliente.getDireccion() + " - " + cliente.getTelefono() + " - "
-                        + cliente.getUser() + " - " + cliente.getPasword());
-                escribir.newLine();
-            }
-            escribir.close();
-        } catch (IOException e) {
-            mostrarAlerta(Alert.AlertType.ERROR, "Error al guardar el archivo.", "Error");
-            System.out.println("" + e);
-        }
+    public ListaVehiculos getListaVehiculos() {
+        return listaVehiculos;
     }
 
     @Override
